5b866c8cb087180633d9dd65b94b4a38c90b3e06
Shaun S <shaun.stuart@gmail.com>
2022-11-21 13:49:26 +1300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "splunk_public_ip" {
+  value = module.splunk.splunk_public_ip
+}
+output "splunk_default_username" {
+  value = module.splunk.splunk_default_username
+}
+output "splunk_default_password" {
+  value = module.splunk.splunk_default_password
+}
